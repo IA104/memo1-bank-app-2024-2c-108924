fa308f54f8501965753c04bcb67010a68bd4ad67
Ian Anca <iananca104@gmail.com>
2024-05-27 11:11:37 -0300

    2. BDD

--- src/main/java/com/aninfo/service/AccountService.java
+++ src/main/java/com/aninfo/service/AccountService.java
@@ -67,6 +67,12 @@ public class AccountService {
         }
 
         Account account = accountRepository.findAccountByCbu(cbu);
+
+        if (sum >= 2000) {
+            double extra = Math.min(500, sum * 0.10);
+            sum += extra;
+        }
+
         account.setBalance(account.getBalance() + sum);
         accountRepository.save(account);
 
